62c42e90efc1178a8db9f688103bc9874719451c
Giordano Temple <giordanotemple@gmail.com>
2023-09-14 08:48:26 -0400

    add date-fns

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,19 @@
 import Layout from '../../components/layout';
+import Head from 'next/head';
+
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
   return {
     props: {
       postData,
     },
   };
 }
+
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -20,14 +25,21 @@ export async function getStaticPaths() {
 
 
 export default function Post({ postData }) {
+  console.log(postData)
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
+
 
